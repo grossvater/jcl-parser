255311537d83944ebe70adc605521d2473fc3294
grossvater <gitpatch@gmail.com>
2017-11-12 15:07:47 +0200

    add test utils

---
+++ org.grossvater.jcl.parser.test/src/main/java/org/grossvater/jcl/parser/ExToken.java
@@ -0,0 +1,25 @@
+package org.grossvater.jcl.parser;
+
+import static org.grossvater.jcl.validator.Args.*;
+
+public class ExToken {
+	public String text;
+	public Integer type;
+	public Integer channel;
+
+	public ExToken(Integer type) {
+		this(type, null, null);
+	}
+
+	public ExToken(Integer type, String text) {
+		this(type, text, null);
+	}
+	
+	public ExToken(Integer type, String text, Integer channel) {
+		notNull(type);
+		
+		this.text = text;
+		this.type = type;
+		this.channel = channel;
+	}
+}
--- org.grossvater.jcl.parser.test/src/main/java/org/grossvater/jcl/parser/TestUtils.java
+++ org.grossvater.jcl.parser.test/src/main/java/org/grossvater/jcl/parser/TestUtils.java
@@ -4,6 +4,12 @@ import java.io.File;
 import java.io.Reader;
 import java.io.StringReader;
 import java.net.URISyntaxException;
+import java.util.Iterator;
+import java.util.List;
+import org.antlr.v4.runtime.Token;
+import org.junit.Assert;
+
+import static org.grossvater.jcl.validator.Args.*;
 
 public class TestUtils {
 	public static String makeFile(String base, String fileName) {
@@ -34,5 +40,36 @@ public class TestUtils {
 		}
 		
 		return new StringReader(b.toString());
+	}
+	
+	public static void assertEquals(List<Token> tokens, List<ExToken> expected) {
+		notNull(tokens);
+		notNull(expected);
+		
+		Assert.assertEquals(expected.size(), tokens.size());
+		
+		Iterator<Token> it = tokens.iterator();
+		Iterator<ExToken> exIt = expected.iterator();
+		int i = 0;
+		
+		while (it.hasNext() && exIt.hasNext()) {
+			Token t = it.next();
+			ExToken et = exIt.next();
+			
+			if (et.type != null) {
+				Assert.assertEquals(String.format("token %d: type failed; expected %d found %d", i, et.type, t.getType()), 
+										          et.type.intValue(), t.getType());
+			}
+			if (et.text != null) {
+				Assert.assertEquals(String.format("token %d: text failed; expected %s found %s", i, et.text, t.getText()),
+										          et.text, t.getText());
+			}
+			if (et.channel != null) {
+				Assert.assertEquals(String.format("token %d: channel failed; expected %d found %d", i, et.channel, t.getChannel()), 
+										          et.channel.intValue(), t.getChannel());
+			}
+			
+			i++;
+		}
 	}
 }
