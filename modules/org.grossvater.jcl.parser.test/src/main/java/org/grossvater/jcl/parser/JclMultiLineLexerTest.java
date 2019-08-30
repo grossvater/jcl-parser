925047b392dffe81b109cbd3252221aec02fe3b8
grossvater <gitpatch@gmail.com>
2019-08-30 14:18:33 +0300

    refactor test

--- modules/org.grossvater.jcl.parser.test/src/main/java/org/grossvater/jcl/parser/JclMultiLineLexerTest.java
+++ modules/org.grossvater.jcl.parser.test/src/main/java/org/grossvater/jcl/parser/JclMultiLineLexerTest.java
@@ -2,27 +2,37 @@ package org.grossvater.jcl.parser;
 
 import org.junit.Test;
 
+import static org.grossvater.jcl.parser.LineUtils.lines;
+
 public class JclMultiLineLexerTest {
     @Test
     public void testSingleFieldId() {
-        AntlrUtils.match("//\n//", new int[] { JclLexer.FIELD_ID, JclLexer.NL, JclLexer.FIELD_ID });
+        AntlrUtils.match(lines("//",
+                               "//"
+                         ), new int[] { JclLexer.FIELD_ID, JclLexer.NL, JclLexer.FIELD_ID });
     }
     
-   @Test
+    @Test
     public void testName() {
-        AntlrUtils.match("//XXX\n//XXX", new int[] { JclLexer.FIELD_ID, JclLexer.FIELD_NAME, JclLexer.NL,
+        AntlrUtils.match(lines("//XXX",
+                               "//XXX"
+                         ), new int[] { JclLexer.FIELD_ID, JclLexer.FIELD_NAME, JclLexer.NL,
                                                      JclLexer.FIELD_ID, JclLexer.FIELD_NAME });
     }
    
    @Test
    public void testOperation() {
-       AntlrUtils.match("//XXX YYY\n//XXX YYY", new int[] { JclLexer.FIELD_ID, JclLexer.FIELD_NAME, JclLexer.BLANK, JclLexer.FIELD_OP, JclLexer.NL,
+       AntlrUtils.match(lines("//XXX YYY",
+                              "//XXX YYY"
+                        ), new int[] { JclLexer.FIELD_ID, JclLexer.FIELD_NAME, JclLexer.BLANK, JclLexer.FIELD_OP, JclLexer.NL,
                                                             JclLexer.FIELD_ID, JclLexer.FIELD_NAME, JclLexer.BLANK, JclLexer.FIELD_OP });
    }
    
    @Test
    public void testPosParam() {
-       AntlrUtils.match("//XXX YYY A\n//XXX YYY A", new int[] { JclLexer.FIELD_ID, JclLexer.FIELD_NAME, JclLexer.BLANK, JclLexer.FIELD_OP,
+       AntlrUtils.match(lines("//XXX YYY A",
+                              "//XXX YYY A"
+                        ), new int[] { JclLexer.FIELD_ID, JclLexer.FIELD_NAME, JclLexer.BLANK, JclLexer.FIELD_OP,
                                                                 JclLexer.BLANK, JclLexer.PARAM_TOKEN, JclLexer.NL,
                                                                 JclLexer.FIELD_ID, JclLexer.FIELD_NAME, JclLexer.BLANK, JclLexer.FIELD_OP,
                                                                 JclLexer.BLANK, JclLexer.PARAM_TOKEN });
@@ -30,7 +40,9 @@ public class JclMultiLineLexerTest {
    
    @Test
    public void testEndComment() {
-       AntlrUtils.match("//X Y A COMM\n// X", new int[] { JclLexer.FIELD_ID, JclLexer.FIELD_NAME, JclLexer.BLANK, JclLexer.FIELD_OP,
+       AntlrUtils.match(lines("//X Y A COMM",
+                              "// X"
+                        ), new int[] { JclLexer.FIELD_ID, JclLexer.FIELD_NAME, JclLexer.BLANK, JclLexer.FIELD_OP,
                                                          JclLexer.BLANK, JclLexer.PARAM_TOKEN, JclLexer.BLANK, JclLexer.COMMENT, JclLexer.NL,
                                                          JclLexer.FIELD_ID, JclLexer.BLANK, JclLexer.COMMENT });
    }
