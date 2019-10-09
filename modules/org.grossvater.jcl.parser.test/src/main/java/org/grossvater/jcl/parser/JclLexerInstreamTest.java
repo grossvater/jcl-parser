fde70cd26efa1ecfc01dc3ed6532691c570de3b3
grossvater <gitpatch@gmail.com>
2019-10-09 11:20:53 +0300

    add new test

--- modules/org.grossvater.jcl.parser.test/src/main/java/org/grossvater/jcl/parser/JclLexerInstreamTest.java
+++ modules/org.grossvater.jcl.parser.test/src/main/java/org/grossvater/jcl/parser/JclLexerInstreamTest.java
@@ -38,6 +38,14 @@ public class JclLexerInstreamTest {
     }
 
     @Test
+    public void testDdData() {
+        AntlrUtils.match(lines("// DD DATA", "free text", "/*"),
+                new int[] { JclLexer.FIELD_ID, JclLexer.BLANK, JclLexer.FIELD_DD, JclLexer.BLANK, JclLexer.PARAM_DD_DATA, JclLexer.NL,
+                        JclLexer.INSTREAM_DATA_LINE, JclLexer.NL,
+                        JclLexer.FIELD_INSTREAM_DELIM });
+    }
+
+    @Test
     public void testDdCustomDelimiter() {
         AntlrUtils.match(lines("// DD *,DLM=XXX", "free text", "XXX"),
                 new int[] { JclLexer.FIELD_ID, JclLexer.BLANK, JclLexer.FIELD_DD, JclLexer.BLANK, JclLexer.PARAM_DD_STAR,
